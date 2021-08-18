a1c9abcadafaeafc4fc55e00ab9f0996a0f9605c
Robert_Lopez <rplopez720@gmail.com>
2021-08-18 19:49:20 -0400

    Add project files.

---
+++ FinalProject.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FinalProject.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ FinalProject.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinalProject.WebAPI.WebApiApplication" Language="C#" %>
