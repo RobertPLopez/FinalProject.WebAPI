b3caff6a5036ed8114d74094c032e88ea682d1f0
Robert_Lopez <rplopez720@gmail.com>
2021-08-24 18:57:08 -0400

    Lopez CRUD Methods added

---
+++ FinalProject.Models/NoteEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class NoteEdit
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
+
