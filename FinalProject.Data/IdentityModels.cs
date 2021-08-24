f11771e0287f6fc7168867fa4c315233fb98a9aa
Robert_Lopez <rplopez720@gmail.com>
2021-08-24 18:08:30 -0400

    Lopez Database Materials

--- FinalProject.Data/IdentityModels.cs
+++ FinalProject.Data/IdentityModels.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -28,6 +31,40 @@ namespace FinalProject.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<Note> Notes { get; set; } //<--- Add this
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+             .Conventions
+             .Remove<PluralizingTableNameConvention>();
+            
+            modelBuilder
+             .Configurations
+             .Add(new IdentityUserLoginConfiguration())
+             .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+
+
+
+public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    //<----Closing curly brace of file
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
