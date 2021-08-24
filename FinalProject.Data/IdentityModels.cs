1d12882168dbb437da0e075c3a1fb7212f75f046
David Behrmann <davidbehrmanntx@gmail.com>
2021-08-24 19:36:39 -0400

    added review table

--- FinalProject.Data/IdentityModels.cs
+++ FinalProject.Data/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -29,5 +30,6 @@ namespace FinalProject.Data
         {
             return new ApplicationDbContext();
         }
+        public DbSet<Reveiw> Reviews { get; set; }
     }
 }
---
+++ FinalProject.Data/Review.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Data
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+        [ForeignKey(nameof(Author))]
+        public int AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        [Required]
+        public string AuthorName { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public double Rating { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        
+
+
+
+    }
+}
