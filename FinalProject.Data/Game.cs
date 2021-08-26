2bb156003686ad5722c23da0dcabcba6a7f06861
Robert_Lopez <rplopez720@gmail.com>
2021-08-26 19:36:31 -0400

    Contoller

--- FinalProject.Data/Game.cs
+++ FinalProject.Data/Game.cs
@@ -5,20 +5,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalProject.Data.Entities
+namespace FinalProject.Data
 {
+    public enum GameGenre
+    {
+        RPG,
+        MMO,
+        Text,
+        "2D",
+        "3D"
+    }
 
     public class Game
     {
-
         [Key]
         public int GameID { get; set; }
         [Required]
         public string DeveloperName { get; set; }
+        [Required]
         public string Description { get; set; }
-        public string Genre { get; set; }
+        [Required]
+        public GameGenre Genre { get; set; }
         [Required]
         public double AverageRating { get; set; }
+        [Required]
         public int AgeOfPlayer { get; set; }
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
---
+++ FinalProject.WebAPI/Controllers/GameController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FinalProject.Data;
+using FinalProject.Models;
+using FinalProject.Services;
+using Microsoft.AspNet.Identity;
+
+namespace FinalProject.WebAPI.Controllers
+{
+    [Authorize]
+    public class GameController : ApiController
+    {
+        private ReactionServices CreateGameServices()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var reactionServices = new GameServices(userId);
+            return reactionServices;
+        }
+
+        public IHttpActionResult Post(GameCreate game)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateGameServices();
+
+            if (!service.CreateGame(game))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                if (ctx.Games.Find(id) is null)
+                    return BadRequest("Invalid Request");
+            }
+
+            var service = CreateGameServices();
+            var games = service.GetGameByGameId(id);
+            return Ok(games);
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateGameServices();
+
+            if (!service.DeleteGame(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(GameEdit game)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateGameServices();
+
+            if (!service.UpdateGame(game))
+                return InternalServerError();
+
+            return Ok();
+
+        }
+    }
+}
